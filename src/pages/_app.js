786e36ce005aac3d6ac1c95f2d7e459c519ef7aa
JJQKR <yjcoyjco@naver.com>
2025-08-24 21:03:46 +0900

    js로 변경, 뷰어 컴포넌트 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,18 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css"; // 이 경로가 맞습니다!
+import Link from "next/link";
+import "../styles/globals.css";
 
-export default function RocketYunju({ Component, pageProps }: AppProps) {
+export default function RocketYunju({ Component, pageProps }) {
   console.log("현재 페이지:", Component.name); // 디버깅용
   console.log("페이지 데이터:", pageProps); // 디버깅용
   return (
     <div>
       <header>
         <nav className="flex flex-col">
-          <a href="/">홈으로 이동</a>
-          <a href="/chapter_1">제 1장 보기</a>
+          <Link href="/">홈으로 이동</Link>
+          <Link href="/chapter_1">제 1장 보기</Link>
         </nav>
       </header>
-      <main>
-        {" "}
+      <main className="font-semibold">
         <Component {...pageProps} />
       </main>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return <div>홈 컴포넌트 내용</div>;
 }
